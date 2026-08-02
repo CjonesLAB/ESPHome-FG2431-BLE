37cbef7354c1d085f64ec689abd345106cd1dfad
Carsten <81263879+CjonesLAB@users.noreply.github.com>
2026-08-02 12:36:36 +0200

    Add custom_components/fg2431_body_metrics/const.py

---
+++ custom_components/fg2431_body_metrics/const.py
@@ -0,0 +1,15 @@
+"""Constants for the FG2431 Body Metrics integration."""
+
+DOMAIN = "fg2431_body_metrics"
+
+CONF_PROFILE_NAME = "profile_name"
+CONF_SEX = "sex"
+CONF_HEIGHT = "height"
+CONF_WEIGHT_ENTITY = "weight_entity"
+CONF_HEART_RATE_ENTITY = "heart_rate_entity"
+CONF_IMPEDANCE_ENTITY = "impedance_entity"
+
+SEX_FEMALE = "female"
+SEX_MALE = "male"
+
+UPDATE_DELAY_SECONDS = 0.35
